274e32da53c4604ceb3444401eff44c2a46d5746
Alan Luo <me@alanluo.com>
2021-02-11 16:44:51 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+type Planet = {
+  id: string;
+  energy: number;
+  lastUpdated: number;
+};
+
+const myPlanet: Planet = {
+  id: "0",
+  energy: 0,
+  lastUpdated: 0,
+};
 
 function App() {
+  const [planet, setPlanet] = useState<Planet>(myPlanet);
+
+  useEffect(() => {
+    console.log("App rerendered");
+  });
+
+  useEffect(() => {
+    console.log("energy updated out of component ", planet.energy);
+  }, [planet.energy]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyComponent planet={planet} />
+      <div>
+        <p>id: {planet.id}</p>
+        <p>energy: {planet.energy}</p>
+      </div>
+      <button
+        onClick={() => {
+          myPlanet.energy += 10;
+          myPlanet.lastUpdated = Date.now();
+          setPlanet(myPlanet);
+        }}
+      >
+        Update Planet
+      </button>
+    </div>
+  );
+}
+
+function MyComponent({ planet }: { planet: Planet }) {
+  useEffect(() => {
+    console.log("energy updated in component ", planet?.energy);
+  }, [planet?.energy]);
+
+  return (
+    <div>
+      <p>id: {planet.id}</p>
+      <p>energy: {planet.energy}</p>
     </div>
   );
 }
